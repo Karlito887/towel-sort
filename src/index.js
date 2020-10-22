e5c17c3542dc5c19a5c57e3d7ebc315f816d6073
Nazarii <56686660+Karlito887@users.noreply.github.com>
2020-10-22 21:17:49 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,16 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    if(!matrix) return []
+    let isDirect = true
+    const sortedMatrix = matrix.map(item => {
+        if(isDirect) {
+            isDirect = false
+            return item
+        } else {
+            isDirect = true
+            return item.reverse()
+        }
+    })
+    return sortedMatrix.flat()
 }
